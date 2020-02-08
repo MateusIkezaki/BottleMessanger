3f120f08b837711a2ecebf763d106852c04bce4a
MateusIkezaki <38020562+MateusIkezaki@users.noreply.github.com>
2020-02-08 00:13:27 -0300

    added signature and made website responsive

--- public/app.js
+++ public/app.js
@@ -1,28 +1,59 @@
 const welcome = document.getElementById("welcome")
+const next = document.getElementById("next")
+const author = document.getElementById("author")
+var canproceed = true
 
-Begin = async display => {
+const Begin = async display => {
     const intro = await fetch('/messages')
     const answer = await intro.json()
-    welcome.textContent = answer[Math.floor(Math.random()*answer.length)].rawdata
+    const contentid = Math.floor(Math.random()*answer.length)
+    welcome.textContent = answer[contentid].rawdata
+    author.textContent = `- ${answer[contentid].nickname}`
+}
+
+const ChangeMessage = () => {
+    next.addEventListener('click', () => {
+        if(canproceed)
+        {
+            welcome.style.opacity = 0
+            next.style.opacity = 0
+            author.style.opacity = 0
+            window.setTimeout('welcome.style.opacity = 1; next.style.opacity = 1; author.style.opacity = 1; Begin()', 2000)
+            window.setTimeout('canproceed = true', 4000)
+            canproceed = false
+        }  
+    })
 }
 
 Begin()
+ChangeMessage()
 
 const button = document.getElementById("send")
         button.addEventListener('click', async event => {
             const rawdata = document.getElementById("textbox").value
-            const processeddata = {rawdata}
-            console.log(processeddata)
-            console.log(JSON.stringify(processeddata))
-            const options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(processeddata)
+            const nickname = document.getElementById("nameplace").value
+            if(rawdata != '')
+            {
+                const processeddata = {rawdata, nickname}
+            
+                console.log(processeddata)
+                console.log(JSON.stringify(processeddata))
+                const options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(processeddata)
+                }
+                const message = await fetch('/messages', options)
+                document.getElementById("textbox").value = ""
+                const response = await message.json()
+                console.log(response)
+                alert("Bottle sent successfully!")
             }
-            const message = await fetch('/messages', options)
-            document.getElementById("textbox").value = ""
-            const response = await message.json()
-            console.log(response)
+            else
+            {
+                alert("Please fill the textbox below")
+            }
+            
         })
